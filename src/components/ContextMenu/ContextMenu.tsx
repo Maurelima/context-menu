bcb03d22bc8e6f228990f67a4836d3bb4523b886
Marco <maurelima@gmail.com>
2022-03-23 11:29:09 -0300

    Refatoring

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -1,62 +1,75 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import './ContextMenu.scss'
 
 type MenuData = {
     menuDescription: string;
-    menuFunction: any;
+    menuFunction: () => void;
+    divider?: boolean;
 }
 
 interface ContextMenuProps {
     /**
-     * Determines whether an array includes a certain element, returning true or false as appropriate.
-     * @param searchElement The element to search for.
-     * @param fromIndex The position in this array at which to begin searching for searchElement.
+     * Replaces browser default contextmenu
+     * @param menuData Array containing the menu properties
+     * * @param menuDescription Menu title
+     * * @param menuFunction Menu action
+     * * @param divider Add a separator after the menu
      */
-    showContextMenu: boolean;
     menuData: MenuData[];
-    anchorPoint: {
-        xClickPosition: number;
-        yClickPosition: number;
-    }
 }
 
-const ContextMenu = ({ showContextMenu, menuData, anchorPoint }: ContextMenuProps) => {
+const ContextMenu: React.FC<ContextMenuProps> = (props) => {
+    const { menuData } = props;
+    const [anchorPoints, setAnchorPoints] = useState({ x: 0, y: 0 });
+    const [show, setShow] = useState(false);
+    const [callableFunction, setCallableFunction] = useState<string>('');
+
+    const handleContextMenu = useCallback(
+        (event) => {
+            event.preventDefault();
+            setAnchorPoints({ x: event.pageX, y: event.pageY });
+            setShow(true);
+        },
+        [setShow, setAnchorPoints]
+    );
+
+    const handleClick = useCallback(() => (show ? setShow(false) : null), [show]);
+
+    useEffect(() => {
+        document.addEventListener("click", handleClick);
+        document.addEventListener("contextmenu", handleContextMenu);
+        return () => {
+            document.removeEventListener("click", handleClick);
+            document.removeEventListener("contextmenu", handleContextMenu);
+        };
+    });
     return (
         <>
-            {showContextMenu && (
+            {show && (
                 <ul
                     className="menu"
                     style={{
-                        top: anchorPoint.yClickPosition,
-                        left: anchorPoint.xClickPosition,
+                        top: anchorPoints.y,
+                        left: anchorPoints.x,
                     }}
                 >
-                    <li>
-                        <button
-                            type="button"
-                        // onClick={() => {
-                        //     listOfFunctions[callableFunction]();
-                        // }}
-                        >
-                            Atualizar este campo
-                        </button>
-                    </li>
-                    <li>
-                        <button type="button"
-                        // onClick={() => listOfFunctions.attTodos()}
-                        >
-                            Atualizar todos os campos
-                        </button>
-                    </li>
-                    <hr className="divider" />
-                    <li>
-                        <button type="button" onClick={() => alert('aaa')}>
-                            Ver diferenças
-                        </button>
-                    </li>
+                    {menuData.map((menu) => {
+                        return (
+                            <>
+                                <li>
+                                    <button type="button" onClick={menu.menuFunction}>
+                                        {menu.menuDescription}
+                                    </button>
+                                </li>
+                                {menu.divider && (
+                                    <hr className="divider" />
+                                )}
+                            </>
+                        )
+                    })}
                 </ul>
-            )} 
+            )}
         </>
 
     )
